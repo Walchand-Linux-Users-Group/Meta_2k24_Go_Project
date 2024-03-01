627c6125577416546e310f2d181d0bfd9d7f0fc4
HimanshuMude <himanshumude01@gmail.com>
2024-03-01 22:47:55 +0530

    Changes Made

--- Features/features.go
+++ Features/features.go
@@ -2,77 +2,91 @@ package features
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 	"strconv"
 	"strings"
-	"fmt"
 )
 
 type Student struct {
-	Name string
+	Name   string
 	RollNo string
-	Marks int
+	Marks  int
 }
 
 type Class struct {
 	Students []Student
 }
 
-func (c *Class) NewClass(){
+// Take input
+func inputName() string {
+	fmt.Println("Enter your first name")
+	var name string
+	fmt.Scanln(&name)
+	return name
+}
 
-	
+func inputRoll() string {
+	fmt.Println("Enter your roll no")
+	var roll string
+	fmt.Scanln(&roll)
+	return roll
+}
+
+func inputMarks() int {
+	fmt.Println("Enter your marks")
+	var marks int
+	fmt.Scanln(&marks)
+	return marks
+}
+
+func (c *Class) NewClass() {
 
 	// fmt.Println(entry)
 	var studentsArray []Student
-	file,err:=os.Open("db.txt")
-	if err !=nil {
+	file, err := os.Open("db.txt")
+	if err != nil {
 		panic(err)
 	}
-	scanner:=bufio.NewScanner(file)
+	scanner := bufio.NewScanner(file)
 
 	for scanner.Scan() {
-		line:=scanner.Text()
-		fields:=strings.Split(line," ")
-		marks,_:=strconv.Atoi(fields[2])
+		line := scanner.Text()
+		fields := strings.Split(line, " ")
+		marks, _ := strconv.Atoi(fields[2])
 
-		entry:=Student{
-			Name: fields[0],
+		entry := Student{
+			Name:   fields[0],
 			RollNo: fields[1],
-			Marks: marks,
+			Marks:  marks,
 		}
 
-		studentsArray=append(studentsArray, entry)
+		studentsArray = append(studentsArray, entry)
 
 	}
 
 	// fmt.Println(studentsArray)
-	c.Students=studentsArray
+	c.Students = studentsArray
 
 }
 
 // CRUD: Create Read Update and Delete
 
-func (c *Class) AddStudent(){
+func (c *Class) AddStudent() {
 
-	fmt.Println("Enter your first name")
-	var name string
-	fmt.Scanln(&name)
+	name := inputName()
 
-	fmt.Println("Enter your roll no")
-	var roll string
-	fmt.Scanln(&roll)
+	roll := inputRoll()
 
-	fmt.Println("Enter your marks")
-	var marks int
-	fmt.Scanln(&marks)
+	marks := inputMarks()
 
-	entry:=Student{
-		Name: name,
+	entry := Student{
+		Name:   name,
 		RollNo: roll,
-		Marks: marks,
+		Marks:  marks,
 	}
 
-	c.Students=append(c.Students, entry)
+	c.Students = append(c.Students, entry)
 
 	fmt.Println("Student Added Successfully")
 	fmt.Println()
@@ -80,42 +94,34 @@ func (c *Class) AddStudent(){
 	//Update this data in the db.txt
 }
 
-func (c *Class) ShowStudents(){
-	for _,student:=range(c.Students){
-		name,roll,marks:=student.Name,student.RollNo,student.Marks
-		fmt.Printf("Name: %s\nRoll No: %s\nMarks: %d\n",name,roll,marks)
+func (c *Class) ShowStudents() {
+	for _, student := range c.Students {
+		name, roll, marks := student.Name, student.RollNo, student.Marks
+		fmt.Printf("Name: %s\nRoll No: %s\nMarks: %d\n", name, roll, marks)
 		fmt.Println()
 	}
 }
 
-func (c *Class) UpdateStudent(){
-	var roll string
-	fmt.Println("Enter the roll no")
-	fmt.Scanln(&roll)
+func (c *Class) UpdateStudent() {
+	roll := inputRoll()
 	var i int
-	for idx,student:=range(c.Students) {
-		if(student.RollNo==roll){
-			i=idx
+	for idx, student := range c.Students {
+		if student.RollNo == roll {
+			i = idx
 			break
 		}
 	}
 
-	fmt.Println("Enter your first name")
-	var name string
-	fmt.Scanln(&name)
+	newName := inputName()
 
-	fmt.Println("Enter your roll no")
-	var rollNo string
-	fmt.Scanln(&rollNo)
+	newRoll := inputRoll()
 
-	fmt.Println("Enter your marks")
-	var marks int
-	fmt.Scanln(&marks)
+	newMarks := inputMarks()
 
-	c.Students[i]=Student{
-		Name: name,
-		RollNo: rollNo,
-		Marks: marks,
+	c.Students[i] = Student{
+		Name:   newName,
+		RollNo: newRoll,
+		Marks:  newMarks,
 	}
 
 	fmt.Println("Data updated successfully")
@@ -124,26 +130,23 @@ func (c *Class) UpdateStudent(){
 	//Update it in db.txt
 }
 
-func (c *Class) DeleteStudent(){
-	var roll string
-	fmt.Println("Enter the roll no")
-	fmt.Scanln(&roll)
+func (c *Class) DeleteStudent() {
+	roll := inputRoll()
 	var i int
-	for idx,student:=range(c.Students) {
-		if(student.RollNo==roll){
-			i=idx
+	for idx, student := range c.Students {
+		if student.RollNo == roll {
+			i = idx
 			break
 		}
 	}
 
-	dummyArr:=c.Students
+	dummyArr := c.Students
 
-	dummyArr=append(dummyArr[:i],dummyArr[i+1:]... )
-	c.Students=dummyArr
+	dummyArr = append(dummyArr[:i], dummyArr[i+1:]...)
+	c.Students = dummyArr
 	fmt.Println("Data deleted successfully")
 	fmt.Println()
 
 	//Update the data in db.txt
 
 }
-
